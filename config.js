f0bcc1ad8cdb33a51918b9dbf944ba66a2e96d9e
Meit Maheshwari <38315313+meit1153@users.noreply.github.com>
2024-12-03 19:13:50 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 module.exports = {
     PORT: process.env.PORT || 3001,
     db: {
-        user: process.env.DB_USER || 'meit',
-        host: process.env.DB_HOST || 'host.docker.internal',
-        database: process.env.DB_NAME || 'restaurant',
-        password: process.env.DB_USER_PASSWORD || 'meit@1991',
+        user: 'meit',
+        host: 'host.docker.internal',
+        database: 'restaurant',
+        password: 'meit@1991',
         port: 5432,
     },
-    TOKEN_SECRET: process.env.TOKEN_SECRET || 'asfdniaudoqnkdkds#7673j2rokjndaskjfb@'
+    TOKEN_SECRET: 'asfdniaudoqnkdkds#7673j2rokjndaskjfb@'
 }
